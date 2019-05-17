cfc07f101d60cfe6875e1e90ffb5486892c98b29
sc4m-ua <47226524+sc4m-ua@users.noreply.github.com>
2019-05-17 09:54:09 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,7 @@
+$(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top-70+"px"});
+                return false;
+        });
+});
